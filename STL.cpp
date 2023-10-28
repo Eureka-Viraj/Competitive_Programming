eeaa9d5e9989432bbac4ab7f6a2b43356b001728
Viraj Deshpande <91491296+Eureka-Viraj@users.noreply.github.com>
2023-10-28 18:33:36 +0530

    Update STL.cpp
    Adding the remaining functions of vector stl container added the functions like add(), end(), begin(), clear(), push_back(), pop_back(), emplace_back(), pair<> with vector, auto, vector<int>:: iterator cy, auto cy, auto cy : v3 all there methods to iterate through a vector

--- STL.cpp
+++ STL.cpp
@@ -5,7 +5,7 @@ void stl_pair(int a, int b){
   //Basic Syntax of the Pair
   pair<int, int> p = {a, b};
   cout << p.first << " " << p.second << endl;
-  // nested pair here we are doing pairs inside pair
+  // nested pair Here we are doing pairs inside pair
   pair<int, pair<int, int>> nesP = {a, {b, a+b}};
   cout << nesP.first << " " << nesP.second.first << " " << nesP.second.second << endl;
   // We can also store the pairs inside the array
@@ -18,7 +18,7 @@ void stl_pair(int a, int b){
 }
 
 void stl_vector(int a, int b){
-  vector<int> v;// declared vector is dynamic in nature and when we don't know the the size of array then we use vector!
+  vector<int> v;// declared vector is dynamic in nature and when we don't know the size of the array then we use vector!
   v.push_back(1);//{1} pushed back 1 element in vector v
   v.emplace_back(2);//{1,2} emplace back(similar to push_back) but it dynamically increases the size & it is faster than push_back inserts 2 at the back
   vector<int> v1(5,100); // -> It will be storing {100, 100, 100, 100, 100}
@@ -34,7 +34,37 @@ void stl_vector(int a, int b){
   vector<int> :: iterator it = v.begin(); // now here v.begin() it is pointing to the address/memory of the 0th index of vector.
   it++;
   cout << *(it); // here * pointers concept is used to get the actual stored values 
-  
+  vector<int> v4(4,10);
+  cout<< v4.back(); // suppose {10,20,30,40} so it will be printing the 40;
+  // different ways to represent iterator
+  // 1st way 
+  for(vector<int> :: iterator cy = v.begin(); cy!=v.end(); cy++){
+    cout << *(cy) << " ";
+  }
+  // so for vector<int> :: iterator cy --> we can write auto cy;
+  // 2nd way
+  for(auto cy - v.begin(); cy!=v.end(); cy++){
+    cout<< *(cy) << " "; 
+  }
+  // 3rd way
+  for(auto cy : v){
+    cout << cy << " ";
+  }
+  v.erase(v.begin()+1) // 2nd element will be deleted
+  v.erase(v.begin()+2, v.begin()+4) // so here 3rd and 4th element will be deleted. 
+// note that: [Start, End) -> not included. let's take an example v = {10,20,30,40,50} v.erase(v.begin()+2, v.begin()+4) 
+    // 30 & 40 elements will be deleted, so we include Start but we don't closely include End
+  // insert function
+  vector<int> v5(2,100); // {100,100}
+  v5.insert(v5.begin(), 300); // {300,100,100}
+  v5.insert(v5.begin()+1, 2,10); // {300,10,10,100,100}
+  //What if you have a vector & you want to insert that vector into some other vector?
+  vector<int> v6(2,50); //{50,50}
+  v5.insert(v5.begin(),v6.begin(),v6.end()); // {50,50,300,10,10,100,100}
+  // pop back
+  v5.pop_back(); // removes the last elements
+  v5.swaps(v6);
+  v5.clear(); // erases the entire vector.
 }
 
 void stl_list();
